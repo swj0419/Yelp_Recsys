1e3d69c46986ebdd4d3dbf79b5b90f944e7cd47b
Jingyue Shen <jingyue_shen@yahoo.com>
2018-12-11 12:13:41 -0800

    update collaborative filtering

--- CollaborativeFiltering/co_filter.py
+++ CollaborativeFiltering/co_filter.py
@@ -1,47 +1,127 @@
 import numpy as np
 import pandas as pd
+from scipy.optimize import minimize
 from preprocess import load_train_set, load_valid_set
-
+import time
 class co_filter():
-    def __init__(self, num_attr = 100, iteration = 100, reg = 1e-5, lr = 1e-2):
-        user_ids, business_ids, rate_matrix, R = load_train_set()
-        self.theta_ = np.random.rand(user_ids.shape[0], num_attr)
-        self.X_ = np.random.rand(business_ids.shape[0],num_attr)
+    def __init__(self, num_attr = 20,num_iters = 20, reg = 1e-3, lr = 3e-2):
+        user_ids_hash, bus_ids_hash, rate_matrix,  R = load_train_set()
+        
+        self.num_users_ = rate_matrix.shape[0]
+        self.num_bus_ = rate_matrix.shape[1]
+        self.num_attr_ = num_attr
+        self.num_iter_ =  num_iters
+        #weights of users 
+        # self.theta_ = np.random.rand(rate_matrix.shape[0], num_attr)
+        #features of movies
+        # self.X_ = np.random.rand(rate_matrix.shape[1],num_attr)
+
         self.rate_matrix_ = rate_matrix
         self.R_ = R
         self.reg_ = reg
         self.lr_ = lr
-        self.iter_ = iteration
-        self.user_ids_ = user_ids
-        self.bus_ids_ = business_ids
-    def compute_cost(self):
-        J  = 1/2 * np.sum(np.square(np.matmul(self.X_, self.theta_.transpose()) - self.rate_matrix_) * self.R_) + self.reg_ / 2 * np.sum(np.square(self.X_)) + self.reg_ / 2 * np.sum(np.square(self.theta_))
-        X_grad = np.matmul(((np.matmul(self.X_, self.theta_.transpose()) - self.rate_matrix_) * self.R_), self.theta_) + self.reg_ * self.X_
-        theta_grad = np.matmul(((np.matmul(self.X_, self.theta_.transpose()) - self.rate_matrix_) * self.R_).transpose(), self.X_) + self.reg_ * self.theta_
+        self.user_ids_hash_ = user_ids_hash
+        self.bus_ids_hash_ = bus_ids_hash
+
+        #find all training entries
+        self.train_idx_ =  np.transpose(np.nonzero(self.R_))
         
-        return J, X_grad, theta_grad
+        #initialize latent variables
+        self.theta_ = np.random.normal(scale = 1./self.num_attr_, size = (self.num_users_, self.num_attr_))
+        self.X_ = np.random.normal(scale = 1. / self.num_attr_, size = (self.num_bus_, self.num_attr_))
+
+        #initialize biases
+        self.b_u_ = np.zeros(self.num_users_)
+        self.b_bus_ = np.zeros(self.num_bus_)
+        self.b_ = np.mean(self.R_[np.where(self.R_ != 0)])
+
+    def compute_cost(self):
+        
+
+        pred_matrix = self.b_ + self.b_u_[:,np.newaxis] + self.b_bus_[np.newaxis,:] + np.matmul(self.theta_, self.X_.transpose())
+
+        
+        J  = 0
+        start_time = time.time()
+        for idx in range(self.train_idx_.shape[0]):
+            i = self.train_idx_[idx][0]
+            j = self.train_idx_[idx][1]
+            J += 1/2 * pow(self.R_[i,j] - self.b_ - self.b_u_[i] - self.b_bus_[j] - pred_matrix[i,j], 2) 
+        
+        J += self.reg_ / 2 * np.sum(np.square(self.X_)) + self.reg_ / 2 * np.sum(np.square(self.theta_))
+
+        finish_time = time.time()
+        print("time of computing cost: {}".format(finish_time - start_time))
+        return J
+        
+        # theta = unrolled_params[:num_user_features].reshape(self.num_users_, self.num_attr_)
+        # X = unrolled_params[num_user_features:].reshape(self.num_bus_,self.num_attr_)
+
+        # J  = 1/2 * np.sum(np.square(np.matmul(theta, X.transpose()) - self.rate_matrix_) * self.R_) + self.reg_ / 2 * np.sum(np.square(X)) + self.reg_ / 2 * np.sum(np.square(theta))
+
+        # return J
     
+    def sgd(self):
+        # num_user_features = self.num_users_ * self.num_attr_
+        
+        # theta = unrolled_params[:num_user_features].reshape(self.num_users_, self.num_attr_)
+        # X = unrolled_params[num_user_features:].reshape(self.num_bus_,self.num_attr_)
+        
+        # X_grad = np.matmul(((np.matmul(theta, X.transpose()) - self.rate_matrix_) * self.R_).transpose(), theta) + self.reg_ * X
+        # theta_grad = np.matmul(((np.matmul(theta, X.transpose()) - self.rate_matrix_) * self.R_), X) + self.reg_ * theta
+        
+        # return np.concatenate((theta_grad,X_grad),axis = None)
+        for idx in range(self.train_idx_.shape[0]):
+            i = self.train_idx_[idx,0]
+            j = self.train_idx_[idx,1]
+            prediction = self.b_ + self.b_u_[i] + self.b_bus_[j] + self.theta_[i,:].dot(self.X_[j,:])
+            e = self.R_[i,j] - prediction
+            
+            #update weights of biases
+            self.b_u_[i] += self.lr_ * (e - self.reg_ * self.b_u_[i])
+            self.b_bus_[j] += self.lr_ * (e - self.reg_ * self.b_bus_[j])
+            #update weights of latent variables
+            self.theta_[i,:] += self.lr_ * (e * self.X_[j,:] - self.reg_ * self.theta_[i,:])
+            self.X_[j,:] += self.lr_ * (e * self.theta_[i,:] - self.reg_ * self.X_[j,:])
+        
     def train(self):
-        #gradient descent
-        for i in range(self.iter_):
-            J, X_grad, theta_grad = self.compute_cost()
-            print("Iteration {}: Cost: {}".format(i,J))
-            self.X_ -= self.lr_ * X_grad
-            self.theta_ -= self.lr_ * theta_grad
+        
+        for iteration in range(self.num_iter_):
+            np.random.shuffle(self.train_idx_)
+            self.sgd()
+            cost  = self.compute_cost()
+            print("Iteration: %d ; cost = %.4f" % (iteration+1, cost))
 
+        # res = minimize(self.compute_cost,init_params,method = 'BFGS', jac = self.compute_grad, options = {'maxiter':200,'disp':True})
+        
+        # num_user_features = self.num_users_ * self.num_attr_
+        # self.learned_theta_ = res.x[:num_user_features].reshape(self.num_users_, self.num_attr_)
+        # self.learned_X_ = res.x[num_user_features:].reshape(self.num_bus_,self.num_attr_)
+        # np.save('learned_theta.npy',self.learned_theta_)
+        # np.save('learned_X.npy',self.learned_X_)        
+        # print(res.message)
 
-    def predict(self):
-        valid_user_ids, valud_bus_ids, valid_y_t = load_valid_set()
-        for i in range(valid_user_ids):
-            u_idx = np.where(self.user_ids_ == valid_user_ids[i])
-            b_idx = np.where(self.bus_ids_ == valud_bus_ids[i])
-            print("prediction score: {}",np.matmul(self.X_[u_idx],self.theta_[b_idx]))
+    def pred_valid(self):
+        rmse = 0
+        valid_user_ids, valid_bus_ids, valid_y_t = load_valid_set()
+        num_validation = valid_user_ids.shape[0]
+        for i in range(num_validation):
+            u_idx = self.user_ids_hash_[valid_user_ids[i]]
+            b_idx = self.bus_ids_hash_[valid_bus_ids[i]]
+            y_pred_i = self.b_ + self.b_u_[u_idx] + self.b_bus_[b_idx] + np.matmul(self.theta_[u_idx,:], self.X_[b_idx,:])
+            rmse += np.square(y_pred_i - valid_y_t[i])
+        
+        rmse = np.sqrt(rmse / num_validation)
+        return rmse
+        
+
 
 
 def main():
     model = co_filter()
     model.train()
-    model.predict()
+    val_rmse = model.pred_valid()
+    print("validation error: {}".format(val_rmse))
 
 if __name__ == "__main__":
     main()
--- CollaborativeFiltering/preprocess.py
+++ CollaborativeFiltering/preprocess.py
@@ -1,24 +1,45 @@
 import numpy as np
 import pandas as pd
-
+import time
 def load_train_set():
-    users_info = pd.read_csv("./users.csv")
-    business_info = pd.read_csv("business.csv")
-    review_info = pd.read_csv("train_reviews.csv")
+    users_info = pd.read_csv("../users.csv")
+    business_info = pd.read_csv("../business.csv")
+    review_info = pd.read_csv("../train_reviews.csv")
     
     business_ids = pd.DataFrame.as_matrix(business_info["business_id"])
     user_ids = pd.DataFrame.as_matrix(users_info["user_id"])
+    user_ids_hash = {}
+    bus_ids_hash = {}
+    for i in range(user_ids.shape[0]):
+        user_ids_hash[user_ids[i]] = i
+    
+    for j in range(business_ids.shape[0]):
+        bus_ids_hash[business_ids[j]] = j
+
     #create a dataframe to store a user business mapping
     rate_matrix = pd.DataFrame(columns = business_ids, index = user_ids,dtype=float)
     rate_matrix = rate_matrix.fillna(0)
+    
     R = np.zeros((user_ids.shape[0], business_ids.shape[0]))
+   
+    start_time = time.time()
 
     for idx, row in review_info.iterrows():
         rate_matrix.at[row["user_id"],row["business_id"]] = row["stars"]
         r_idx = np.where(user_ids == row["user_id"])[0][0]
         c_idx = np.where(business_ids == row["business_id"])[0][0]
         R[r_idx][c_idx] = 1
-    print(rate_matrix[0:3])
+    
+    finish_time = time.time()
+    print("time to fill rate matrix and R matrix: {}".format(finish_time - start_time))
+    rate_matrix = pd.DataFrame.as_matrix(rate_matrix)
+    print("shape of rate matrix: ")
+    print(rate_matrix.shape)
+    print("first row of rate matrix: ")
+    print(rate_matrix[0])
+   
+
+    print("shape of R matrix")
     #create a hash table for {user : {business_id : rate}}
     # user_hash = {}
     # bus_hash = {}
@@ -35,10 +56,10 @@ def load_train_set():
     #     else:
     #         bus_hash[business_id] = {user_id : row["star"]}
 
-    return user_ids, business_ids, rate_matrix, R
+    return user_ids_hash, bus_ids_hash, rate_matrix, R
 
 def load_valid_set():
-    valid_set = pd.DataFrame.as_matrix(pd.read_csv("./validate_queries.csv"))
+    valid_set = pd.DataFrame.as_matrix(pd.read_csv("../validate_queries.csv"))
     user_ids = valid_set[:,1]
     bus_ids = valid_set[:,2]
     y_t = valid_set[:,3]
@@ -48,6 +69,7 @@ def load_valid_set():
 
 def main():
     load_valid_set()
+    # load_train_set()
 
 if __name__ == "__main__":
     main()
